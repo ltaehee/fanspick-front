77fe7fd86806900cf44612c26058ac5eedabf1e9
yunzzng <leejiyun0114@naver.com>
2025-01-08 02:09:15 +0900

    헤더 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,57 +1,18 @@
-import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useUserContext } from '../context/UserContext'; 
 import styles from '../css/header.module.css';
 import noticeIcon from '/icons/notice.png';
 import cartIcon from '/icons/cart_icon.png';
 import defaultProfile from '/icons/user_icon.png';
 import fanspickLogo from '/icons/user_header_logo.png';
-import api from '../utils/api';
-
-interface User {
-    id: string;
-    name: string;
-    role?: string;
-    profileImage?: string;
-}
+import userMenu from '../consts/userMenu';
+import managerMenu from '../consts/managerMenu';
 
 const Header = () => {
-    const [user, setUser] = useState<User | null>(null);
+    const { user, logout } = useUserContext(); 
     const navigate = useNavigate();
 
-    const userMenu = [
-        { label: "홈", path: "/" },
-        { label: "그립톡", path: "/griptok-category" },
-        { label: "의류", path: "/clothing-category" },
-        { label: "문구", path: "/stationery-category" },
-        { label: "케이스", path: "/case-category" },
-    ];
-
-    const managerMenu = [
-        { label: "홈", path: "/manager-home" },
-        { label: "상품 등록", path: "/product-management" },
-        { label: "상품 조회", path: "/order-management" },
-    ];
-
-    // 로그인된 유저가 있을 경우 로컬스토리지에서 가져오기
-    useEffect(() => {
-        const storedUser = localStorage.getItem("user");
-        if (storedUser) {
-            const userData = JSON.parse(storedUser);
-            setUser(userData);
-        }
-    }, []); 
-
-    // 로그아웃 처리
-    const handleLogout = async () => {
-        try {
-            await api.post("/oauth/logout");
-            setUser(null);  
-            localStorage.removeItem("user");
-            navigate("/login");
-        } catch (err) {
-            console.error("로그아웃 실패:", err);
-        }
-    };
+    console.log("현재 유저 상태:", user); 
 
     // 메뉴 렌더링
     const renderMenu = () => {
@@ -94,7 +55,7 @@ const Header = () => {
                 <div className={styles.icons}>
                     {user ? (
                         <>
-                            <span className={styles.logoutText} onClick={handleLogout}>
+                            <span className={styles.logoutText} onClick={logout}>
                                 logout
                             </span>
                             <a onClick={() => navigate("/profile")}>
---
+++ src/consts/userMenu.ts
@@ -0,0 +1,9 @@
+const userMenu = [
+    { label: "홈", path: "/" },
+    { label: "그립톡", path: "/griptok-category" },
+    { label: "의류", path: "/clothing-category" },
+    { label: "문구", path: "/stationery-category" },
+    { label: "케이스", path: "/case-category" },
+];
+
+export default userMenu;
