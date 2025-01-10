f439e48b6f2fd0b15d09a416d1e319e17bb4cdf0
seyoon <se981106@naver.com>
2025-01-10 23:09:59 +0900

    vite 빌드 시 콘솔 로그 제거 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import removeConsole from 'vite-plugin-remove-console';
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   server: {
     proxy: {
       // /api 경로는 rewrite 적용
